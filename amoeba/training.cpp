9ce10f794bdc5e47be012381d77cc92c408bad94
Thomas Choquet <choquet@irvinesystems.co.jp>
2026-08-30 23:40:09 +0900

    Revert "Fix self-play progress reporting for non-TTY output"
    This reverts commit 65c4b216047718effea902e31c21b5f048697a79.

--- amoeba/training.cpp
+++ amoeba/training.cpp
@@ -129,20 +129,18 @@ void report(std::format_string<Args...> format, Args&&... args)
 
 void reportSelfPlayStatus(uint64_t receivedRounds)
 {
+    selfPlayStatus.rounds += receivedRounds;
     selfPlayStatus.totalRounds += receivedRounds;
     const double elapsed = std::chrono::duration<double>(
         std::chrono::steady_clock::now() - selfPlayStatus.startedAt).count();
     selfPlayStatus.roundsPerSecond = selfPlayStatus.totalRounds / elapsed;
     selfPlayStatus.visible = true;
     if (isatty(STDOUT_FILENO))
-    {
-        selfPlayStatus.rounds += receivedRounds;
         drawSelfPlayStatus();
-    }
     else
     {
         std::println("[selfplay] received {} rounds, {:.1f} rounds/s average",
-                     receivedRounds, selfPlayStatus.roundsPerSecond);
+                     selfPlayStatus.rounds, selfPlayStatus.roundsPerSecond);
         std::fflush(stdout);
     }
 }
